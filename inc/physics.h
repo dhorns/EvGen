688b19e154f060d295be40b68dd30111f4a733e3
David Hornidge <dhornidge@mta.ca>
2015-05-20 13:09:17 +0200

    Adding physics.h

--- inc/physics.h
+++ inc/physics.h
@@ -1,1 +1,41 @@
-/Users/dave/acqu/includes/physics.h
+const Double_t kPI = TMath::Pi();
+const Double_t kD2R = TMath::DegToRad();
+
+const Double_t kHPLANCK_MKS = 6.6260755e-34;
+const Double_t kHPLANCK_EV = 4.1356692e-15;
+const Double_t kHBAR = 1.05457266e-34;
+const Double_t kHBAR_EV = 6.5821220e-16;
+const Double_t kHBARC = 197.327054;
+const Double_t kC = 2.99792458e8;
+const Double_t kC_NM = 0.299792458;
+
+const Double_t kALPHA = 7.29735308e-3;
+const Double_t kECHARGE = 1.60217733e-19;
+const Double_t kR_E = 2.81794092e-15;
+
+const Double_t kME_MKS = 9.1093897e-31;
+const Double_t kME_MEV = 0.5109906;
+const Double_t kMMU_MEV = 105.658389;
+const Double_t kMPI_MEV = 139.5673;
+const Double_t kMPI0_MEV = 134.9630;
+const Double_t kMETA_MEV = 548.8;
+const Double_t kMK_MEV = 493.667;
+const Double_t kMKO_MEV = 497.67;
+const Double_t kMD_MEV = 1869.4;
+const Double_t kMDO_MEV = 1864.7;
+const Double_t kMP_MEV = 938.27231;
+const Double_t kMP_MKS = 1.6726231e-27;
+const Double_t kMN_MEV = 939.56563;
+const Double_t kMN_MKS = 1.6749286e-27;
+const Double_t kMDEUT_MKS = 3.3435860e-27;
+const Double_t kMAMU_MKS = 1.6605402e-27;
+const Double_t kMAMU_MEV = 931.49432;
+const Double_t kM_H1_MEV = 938.259216;
+const Double_t kM_H2_MEV = 1875.587280;
+const Double_t kM_DEUT_MEV = 1875.61339;
+const Double_t kM_HE3_MEV = 2809.414814;
+const Double_t kM_HE4_MEV = 3727.837891;
+const Double_t kM_C12_MEV  = 11174.706055;
+const Double_t kM_O16_MEV  = 14903.90912;
+const Double_t kM_O15_MEV  = 13979.71345;
+const Double_t kEB_DEUT_MEV = 2.22455001;
